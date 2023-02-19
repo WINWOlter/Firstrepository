a535dde73a4b6acd3bcb7bb90609c6b1bf5a8dc5
WNWolter <113820891+WINWOlter@users.noreply.github.com>
2023-02-19 08:19:33 +0000

    добавили конструктор

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ class Person:
   name = "Mark"
   is_male = True
 
+  def __init__(self,surname):
+    self.surname = surname
+    print(self.name)
+    
 
-me = Person ()
-you = Person ()
+
+me = Person ("Kvich")
+you = Person ("Test")
 
 print(me.age)
 me.age += 1
 print(me.age)
+
+# print(me.surname)
+# print(you.surname)
